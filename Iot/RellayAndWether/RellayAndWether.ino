7857d84eebb76928bdf69ad78f581fd4d21aea2d
Alireza Sejdei <alireza.sejdei@gmail.com>
2024-04-16 00:10:14 +0330

    Update ESP8266 Device to support sending variables and sync with relays status

--- Iot/RellayAndWether/RellayAndWether.ino
+++ Iot/RellayAndWether/RellayAndWether.ino
@@ -15,7 +15,30 @@ const int relay_pin4 = 14;
 
 char ssid[] = "Mr.Me";
 char pass[] = "123456789";
+char identifier[] = "test_devide_id_1234";
+char serverUrl[] = "http://5.201.152.69:5264";
 
+
+char val1[] = "val1[]";
+char val2[] = "val2[]";
+char val3[] = "val3[]";
+char val4[] = "val4[]";
+char val5[] = "val5[]";
+char val6[] = "val6[]";
+char val7[] = "val7[]";
+char val8[] = "val8[]";
+char val9[] = "val9[]";
+char val10[] = "val10[]";
+char val11[] = "val11[]";
+char val12[] = "val12[]";
+char val13[] = "val13[]";
+char val14[] = "val14[]";
+char val15[] = "val15[]";
+char val16[] = "val16[]";
+char val17[] = "val17[]";
+char val18[] = "val18[]";
+char val19[] = "val19[]";
+char val20[] = "val20[]";
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -62,9 +85,35 @@ void loop() {
     WiFiClient client;  // Create a WiFiClient object
     HTTPClient http;
 
-    http.begin(client, "http://5.201.152.69:7111/manager-device");
+    http.begin(client, String(serverUrl) + "/device-manager/update-variables/" + String(identifier));
     http.addHeader("Content-Type", "application/json");
-    String jsonData = "{\"temperature\": " + String(temperature) + ",\"humidity\": " + String(humidity) + "}";
+
+    StaticJsonDocument<22> doc;
+    doc["temperature"] = String(temperature);
+    doc["humidity"] = String(humidity);
+    doc["val1"] = val1;
+    doc["val2"] = val2;
+    doc["val3"] = val3;
+    doc["val4"] = val4;
+    doc["val5"] = val5;
+    doc["val6"] = val6;
+    doc["val7"] = val7;
+    doc["val8"] = val8;
+    doc["val9"] = val9;
+    doc["val10"] = val10;
+    doc["val11"] = val11;
+    doc["val12"] = val12;
+    doc["val13"] = val13;
+    doc["val14"] = val14;
+    doc["val15"] = val15;
+    doc["val16"] = val16;
+    doc["val17"] = val17;
+    doc["val18"] = val18;
+    doc["val19"] = val19;
+    doc["val20"] = val20;
+
+    String jsonData;
+    serializeJson(doc, jsonData);
 
     int httpResponseCode = http.POST(jsonData);
     Serial.println("JSON Payload: " + jsonData);
@@ -76,6 +125,7 @@ void loop() {
       Serial.println(http.errorToString(httpResponseCode).c_str());
     }
 
+    http.begin(client, String(serverUrl) + "/device-manager/status/" + String(identifier));
     int httpCode = http.GET();  // Make the HTTP GET request
 
     if (httpCode > 0) {                   // Check for the returning code
