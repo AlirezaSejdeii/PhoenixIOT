aab4718d5c1f2a16a5828f0b4f89267b908df3f2
Alireza Sejdei <alireza.sejdei@gmail.com>
2024-05-12 17:15:44 +0330

    update firmware code

--- Iot/RellayAndWether/RellayAndWether.ino
+++ Iot/RellayAndWether/RellayAndWether.ino
@@ -2,6 +2,10 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 
 #define DHTPIN D4
 const int relay_pin1 = 5;
@@ -17,7 +21,6 @@ char ssid[] = "Mr.Me";
 char pass[] = "123456789";
 char identifier[] = "test_devide_id_1234";
 char serverUrl[] = "http://5.201.152.69:5264";
-
 
 char val1[] = "val1[]";
 char val2[] = "val2[]";
@@ -41,6 +44,15 @@ char val19[] = "val19[]";
 char val20[] = "val20[]";
 
 DHT dht(DHTPIN, DHTTYPE);
+
+
+void fill_with_random_number(char *arr, int size, int max_value) {
+  // Generate a random number between 0 and max_value (inclusive)
+  int random_num = rand() % (max_value + 1);
+
+  // Convert the random number to string format and store it in the character array
+  snprintf(arr, size, "%d", random_num);
+}
 
 void setup() {
   Serial.begin(115200);
@@ -66,6 +78,33 @@ void setup() {
 
 void loop() {
   delay(2000);
+
+  // خط 84 تا 108 برای پر کردن متغفیر ها با مقدار تستی است.
+  // کافیست فقط مقدار هر متغیر را با دیتای دریافتی از سنسور پر کنید
+  const int max_value = 15;
+  srand(static_cast<unsigned int>(time(0)));
+  fill_with_random_number(val1, sizeof(val1), max_value);
+  fill_with_random_number(val2, sizeof(val2), max_value);
+  fill_with_random_number(val3, sizeof(val3), max_value);
+  fill_with_random_number(val4, sizeof(val4), max_value);
+  fill_with_random_number(val5, sizeof(val5), max_value);
+  fill_with_random_number(val6, sizeof(val6), max_value);
+  fill_with_random_number(val7, sizeof(val7), max_value);
+  fill_with_random_number(val8, sizeof(val8), max_value);
+  fill_with_random_number(val9, sizeof(val9), max_value);
+  fill_with_random_number(val10, sizeof(val10), max_value);
+  fill_with_random_number(val11, sizeof(val11), max_value);
+  fill_with_random_number(val12, sizeof(val12), max_value);
+  fill_with_random_number(val13, sizeof(val13), max_value);
+  fill_with_random_number(val14, sizeof(val14), max_value);
+  fill_with_random_number(val15, sizeof(val15), max_value);
+  fill_with_random_number(val16, sizeof(val16), max_value);
+  fill_with_random_number(val17, sizeof(val17), max_value);
+  fill_with_random_number(val18, sizeof(val18), max_value);
+  fill_with_random_number(val19, sizeof(val19), max_value);
+  fill_with_random_number(val20, sizeof(val20), max_value);
+
+
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
 
@@ -82,7 +121,7 @@ void loop() {
   Serial.println("°C ");
 
   if (WiFi.status() == WL_CONNECTED) {
-    WiFiClient client;  // Create a WiFiClient object
+    WiFiClient client;
     HTTPClient http;
 
     http.begin(client, String(serverUrl) + "/device-manager/update-variables/" + String(identifier));
@@ -91,26 +130,70 @@ void loop() {
     StaticJsonDocument<22> doc;
     doc["temperature"] = String(temperature);
     doc["humidity"] = String(humidity);
-    doc["val1"] = val1;
-    doc["val2"] = val2;
-    doc["val3"] = val3;
-    doc["val4"] = val4;
-    doc["val5"] = val5;
-    doc["val6"] = val6;
-    doc["val7"] = val7;
-    doc["val8"] = val8;
-    doc["val9"] = val9;
-    doc["val10"] = val10;
-    doc["val11"] = val11;
-    doc["val12"] = val12;
-    doc["val13"] = val13;
-    doc["val14"] = val14;
-    doc["val15"] = val15;
-    doc["val16"] = val16;
-    doc["val17"] = val17;
-    doc["val18"] = val18;
-    doc["val19"] = val19;
-    doc["val20"] = val20;
+    char jsonBuffer[100];
+
+    
+    //اسم هر متغیر را با مقدار دلخواه پر کنید تا در صفحه با همان نام نشان داده شود
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val1);
+    doc["val1"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val2);
+    doc["val2"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val3);
+    doc["val3"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val4);
+    doc["val4"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val5);
+    doc["val5"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val6);
+    doc["val6"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val7);
+    doc["val7"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val8);
+    doc["val8"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val9);
+    doc["val9"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val10);
+    doc["val10"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val11);
+    doc["val11"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val12);
+    doc["val12"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val13);
+    doc["val13"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val14);
+    doc["val14"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val15);
+    doc["val15"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val16);
+    doc["val16"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val17);
+    doc["val17"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val18);
+    doc["val18"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val19);
+    doc["val19"] = jsonBuffer;
+
+    sprintf(jsonBuffer, "{\"name\":\"temp\",\"value\":\"%s\",\"max\":\"20\",\"min\":\"0\"}", val20);
+    doc["val20"] = jsonBuffer;
 
     String jsonData;
     serializeJson(doc, jsonData);
