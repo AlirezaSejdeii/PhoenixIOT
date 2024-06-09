80062f5a563979e74da51d76f9397c1b992430e1
Alireza Sejdei <alireza.sejdei@gmail.com>
2024-06-09 12:20:32 +0330

    add values limit

---
+++ PhoenixIot.Infrastructure/Migrations/20240608203753_AddSensorLimitValues.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhoenixIot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSensorLimitValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "water_switch_off_from",
+                table: "Devices",
+                newName: "whether_temperature_limit");
+
+            migrationBuilder.RenameColumn(
+                name: "fan_switch_on_at",
+                table: "Devices",
+                newName: "whether_humidity_limit");
+
+            migrationBuilder.RenameColumn(
+                name: "fan_switch_off_at",
+                table: "Devices",
+                newName: "soil_humidity_limit");
+
+            migrationBuilder.AddColumn<int>(
+                name: "light_brightness_limit",
+                table: "Devices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "light_brightness_limit",
+                table: "Devices");
+
+            migrationBuilder.RenameColumn(
+                name: "whether_temperature_limit",
+                table: "Devices",
+                newName: "water_switch_off_from");
+
+            migrationBuilder.RenameColumn(
+                name: "whether_humidity_limit",
+                table: "Devices",
+                newName: "fan_switch_on_at");
+
+            migrationBuilder.RenameColumn(
+                name: "soil_humidity_limit",
+                table: "Devices",
+                newName: "fan_switch_off_at");
+        }
+    }
+}
